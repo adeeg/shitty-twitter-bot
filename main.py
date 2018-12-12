03183f66fe0fa99f776701295de612e1d53118a6
Nenivar <nenivar@gmail.com>
2018-12-12 11:03:31 +0000

    comm with twitter

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ with open('auth.json') as f:
     auth_data = json.load(f)
     f.close()
 
-print(auth_data)
+cons_key = auth_data['consumer_key']
+cons_sec = auth_data['consumer_secret']
+acc_tok = auth_data['access_token']
+acc_tok_sec = auth_data['access_token_secret']
+
+auth = tweepy.OAuthHandler(cons_key, cons_sec)
+auth.set_access_token(acc_tok, acc_tok_sec)
+api = tweepy.API(auth)
+
+user = api.me()
+print(user.name)
+
+# get opt-in from user
