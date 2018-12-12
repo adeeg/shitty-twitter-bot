190ce00ae6ab6f6df79f126531269a819028cf3d
Nenivar <nenivar@gmail.com>
2018-12-12 17:56:16 +0000

    doesn't work bc duplicate messages

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # shitty-twitter-bot
+
+## Running
+
+* make sure the twitter account for your bot can receive direct messages
+* get API keys for your [bot](developer.twitter.com)
+* create & fill in `auth.json`
+
+```
+{
+    "twitter_handle": "",
+    "consumer_key": "",
+    "consumer_secret": "",
+    "access_token": "",
+    "access_token_secret": ""
+}
+```
+
+* `pip install -r requirements.txt`
+* `python3 main.py`
+
+## Using
+
+Tweets from users (in users.txt) will be replied to with a given message.
+
+To opt out, tweet the bot '@nenivar, I would like to opt-out!'
+
+Opting in does not work because you can only have one stream per bot >:(
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tweepy
 import json
-import pickle
+from threading import Lock
 
 """
     read in auth data
@@ -17,8 +17,10 @@ cons_sec = auth_data['consumer_secret']
 acc_tok = auth_data['access_token']
 acc_tok_sec = auth_data['access_token_secret']
 
-OPT_IN = '@{}, I would like to opt-in!'.format(handle)
-OPT_OUT = '@{}, I would like to opt-out!'.format(handle)
+MSG_OPT_IN = '@{}, I would like to opt-in!'.format(handle)
+MSG_OPT_OUT = '@{}, I would like to opt-out!'.format(handle)
+MSG_AT = '@{}'.format(handle)
+MSG_REPLY = 'Here\'s your custom message!'
 
 """
     get opt-in from user
@@ -34,7 +36,7 @@ with open('users.txt', 'r') as f:
 
 def addUserToFile(userId):
     with open('users.txt', 'a') as f:
-        f.write('\n{}'.format(userId))
+        f.write('{}\n'.format(userId))
         f.close()
 
 def removeUserFromFile(userId):
@@ -60,7 +62,6 @@ def removeUser(userId):
 """
     tweepy stuff
 """
-
 auth = tweepy.OAuthHandler(cons_key, cons_sec)
 auth.set_access_token(acc_tok, acc_tok_sec)
 api = tweepy.API(auth)
@@ -68,34 +69,41 @@ api = tweepy.API(auth)
 user = api.me()
 print('Bot running on {}'.format(user.name))
 
-# on any user tweeting @
-class StreamAuth(tweepy.StreamListener):
+class StreamBoth(tweepy.StreamListener):
+    def __init__(self):
+        super().__init__()
+        self.restart = False
+
+    def on_connect(self):
+        print('Streaming started with users {}'.format(users))
+    
     def on_status(self, status):
-        """ if status == OPT_IN:
-            print('opt in!') """
-        print(status.user['id_str'])
+        # for user opting in/out
+        if MSG_AT in status.text:
+            print('{} tweeted you!'.format(status.user.name))
+            if status.text == MSG_OPT_IN:
+                print('{} opted in!'.format(status.user.name))
+                addUser(user)
+                self.restart = True
+            elif status.text == MSG_OPT_OUT:
+                print('{} opted out!'.format(status.user.name))
+                removeUser(user)
+                self.restart = True
+        # for tracked user tweeting
+        elif status.user.id_str in users:
+            print('Tweeted @{}'.format(status.user.name))
+            api.update_status('@{} {}'.format(status.user.name, MSG_REPLY))
 
-# on user tweeting anything
-# add/remove them to users list
-class StreamReply(tweepy.StreamListener):
-    def on_status(self, status):
-        #addUser(status.user.id_str)
-        user = status.user.id_str
-        if status.text == OPT_IN:
-            print('{} opted in!'.format(status.user.name))
-            addUser(user)
-        elif status.text == OPT_OUT:
-            print('{} opted out!'.format(status.user.name))
-            removeUser(user)
+streamList = StreamBoth()
+stream = tweepy.Stream(auth = api.auth, listener=streamList)
+stream.filter(track=MSG_AT, follow=users, is_async=True)
 
-def startStream():
-    sr = StreamReply()
-    s2 = tweepy.Stream(auth = api.auth, listener=sr)
-    s2.filter(track='nenivar', is_async=True)
-    s2.disconnect()
+while 1:
+    if streamList.restart:
+        print('User opted in/out; restarting...')
+        stream.disconnect()
 
-    """sa = StreamAuth()
-    s1 = tweepy.Stream(auth = api.auth, listener=sa)
-    s1.filter(follow=['2286822205'], is_async=True) """
-
-startStream()
+        # restart with new set of users
+        stream = StreamBoth()
+        stream = tweepy.Stream(auth = api.auth, listener=streamList)
+        stream.filter(track=MSG_AT, follow=users, is_async=True)
